c584564bc9bcf29df043dc75b5e9379ea3de32f0
YumeMichi <do4suki@gmail.com>
2021-10-24 18:41:19 +0800

    Revert "Delete vendor class files added for Data"
    For R blobs compatibility
    
    This reverts commit 00d66d64a1920bcacd6912f0c3701def19a5bff3.
    
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Change-Id: Ia91d6271796d70a9f49ee9e6e9e6a2351f0d60fc

--- src/java/com/android/internal/telephony/PhoneSwitcher.java
+++ src/java/com/android/internal/telephony/PhoneSwitcher.java
@@ -101,7 +101,7 @@ public class PhoneSwitcher extends Handler {
     protected static final boolean VDBG = false;
 
     private static final int DEFAULT_NETWORK_CHANGE_TIMEOUT_MS = 5000;
-    protected static final int MODEM_COMMAND_RETRY_PERIOD_MS     = 5000;
+    private static final int MODEM_COMMAND_RETRY_PERIOD_MS     = 5000;
     // After the emergency call ends, wait for a few seconds to see if we enter ECBM before starting
     // the countdown to remove the emergency DDS override.
     @VisibleForTesting
@@ -267,8 +267,8 @@ public class PhoneSwitcher extends Handler {
     public static final int EVENT_PRECISE_CALL_STATE_CHANGED      = 109;
     private static final int EVENT_NETWORK_VALIDATION_DONE        = 110;
     private static final int EVENT_REMOVE_DEFAULT_NETWORK_CHANGE_CALLBACK = 111;
-    protected static final int EVENT_MODEM_COMMAND_DONE             = 112;
-    protected static final int EVENT_MODEM_COMMAND_RETRY            = 113;
+    private static final int EVENT_MODEM_COMMAND_DONE             = 112;
+    private static final int EVENT_MODEM_COMMAND_RETRY            = 113;
     @VisibleForTesting
     public static final int EVENT_DATA_ENABLED_CHANGED            = 114;
     // An emergency call is about to be originated and requires the DDS to be overridden.
@@ -314,7 +314,7 @@ public class PhoneSwitcher extends Handler {
     private ConnectivityManager mConnectivityManager;
     private int mImsRegistrationTech = REGISTRATION_TECH_NONE;
 
-    protected List<Set<CommandException.Error>> mCurrentDdsSwitchFailure;
+    private List<Set<CommandException.Error>> mCurrentDdsSwitchFailure;
 
     private class DefaultNetworkCallback extends ConnectivityManager.NetworkCallback {
         public int mExpectedSubId = SubscriptionManager.INVALID_SUBSCRIPTION_ID;
@@ -571,7 +571,7 @@ public class PhoneSwitcher extends Handler {
         }
     };
 
-    protected boolean isSimApplicationReady(int slotIndex) {
+    private boolean isSimApplicationReady(int slotIndex) {
         if (!SubscriptionManager.isValidSlotIndex(slotIndex)) {
             return false;
         }
@@ -1680,7 +1680,7 @@ public class PhoneSwitcher extends Handler {
         pw.decreaseIndent();
     }
 
-    protected boolean isAnyVoiceCallActiveOnDevice() {
+    private boolean isAnyVoiceCallActiveOnDevice() {
         boolean ret = mPhoneIdInVoiceCall != SubscriptionManager.INVALID_PHONE_INDEX;
         log("isAnyVoiceCallActiveOnDevice: " + ret);
         return ret;
